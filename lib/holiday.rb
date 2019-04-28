4430d5108b75502b0d5b9938eeb0c83a8157f2c7
Jess Rezac <jess.rezac@gmail.com>
2019-04-28 23:58:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     bbq_seasons = []
     holiday_hash[season].each do |holiday, supplies|
-      holiday_supplies = holiday_hash[season][holiday]
+      holiday_supplies = holiday_hash[season][holiday][supplies]
       if supplies.include?("BBQ")
         bbq_seasons << holiday
       end
